5d3db222da48b7b724587929ec257c7bfaa7d445
Andrew Kilpatrick <andrew@kilpatrickaudio.com>
2020-07-14 02:53:27 -0400

    Reverb part of V103 is working.

--- src/utils/dsp_utils.h
+++ src/utils/dsp_utils.h
@@ -27,6 +27,10 @@
 // absolute value
 #define DSP_UTILS_ABS(x) ((x)<0 ? -(x) : (x))
 
+// clamp a value to -1.0 to +1.0
+#define DSP_UTILS_CLAMP(x) (((x) > (1.0)) ? (1.0) : \
+    (((x) < (-1.0)) ? (-1.0) : (x)))
+
 // clamp a value to 0.0 to 1.0
 #define DSP_UTILS_CLAMP_POS(x) (((x) > (1.0)) ? (1.0) : \
     (((x) < (0.0)) ? (0.0) : (x)))
@@ -54,6 +58,109 @@ do { \
     (out) = (in) - (inhist) + (0.999 * (outhist)); \
     (inhist) = (in); \
     (outhist) = (out); \
+})
+
+//
+// 1 pole filter
+//
+// set the coefficient for a single pole low pass filter
+// cutoff - cutoff freq in Hz
+// a0 - zero coeff
+#define DSP_UTILS_FILTER1_SET_CUTOFF(cutoff, a0) ({ \
+    (a0) = 1.0 - expf(-2.0 * M_PI * ((cutoff) / (float)AUDIO_FS)); \
+})
+
+// run the low pass for one sample
+// in - input sample
+// out - output sample - must be different than in
+// a0 - zero coeff
+// z1 - history reg - must be retained between runs
+#define DSP_UTILS_FILTER1_RUN_LPF(in, out, a0, z1) ({ \
+    (out) = (z1) = (((in) - (z1)) * (a0)) + (z1);  \
+})
+
+// run the high pass for one sample
+// in - input sample
+// out - output sample - must be different than in
+// a0 - zero coeff
+// z1 - history reg - must be retained between runs
+#define DSP_UTILS_FILTER1_RUN_HPF(in, out, a0, z1) ({ \
+    (out) = (z1) = (((in) - (z1)) * (a0)) + (z1);  \
+    (out) = (in) - (out); \
+})
+
+//
+// delay memory
+//
+// rotate the delay memory pointer
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+#define DSP_UTILS_ROTATE_DELAY(delayp, delay_len) ({ \
+    (delayp) = ((delayp) - 1) & ((delay_len) - 1); \
+})
+
+// read a sample from a delay line - single sample
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+// addr - the address to read from
+// out - the output var
+#define DSP_UTILS_READ_DELAY(delay, delayp, delay_len, addr, out) ({ \
+    (out) = delay[((delayp) + (addr)) & ((delay_len) - 1)]; \
+})
+
+// read a sample from the delay line - fraction sample interpolated
+// make sure there are enough samples so you don't run off the end
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+// addr - the address to read from as a float - real = addr, fract = interp
+// out - the output var
+#define DSP_UTILS_READ_DELAY_FRACT(delay, delayp, delay_len, addr, out) ({ \
+    (it1) = (addr) - (int)(addr); \
+    (out) = delay[((delayp) + (int)(addr)) & ((delay_len) - 1)] * (1.0 - (it1)); \
+    (out) += delay[((delayp) + ((int)(addr) + 1)) & ((delay_len) - 1)] * (it1); \
+})
+
+// write a sample to the delay line
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+// addr - the address to write to
+// in - the input var
+#define DSP_UTILS_WRITE_DELAY(delay, delayp, delay_len, addr, in) ({ \
+    delay[((delayp) + (addr)) & ((delay_len) - 1)] = (in); \
+})
+
+// run a first-order all-pass filter
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+// inaddr - the address to write to
+// outaddr - the address to read from
+// g - AP feedback coeff - + = alternating sign, - = same sign
+// acc - used for input and output
+// it1 - internal temp var - does not need to be retained
+#define DSP_UTILS_ALLPASS(delay, delayp, delay_len, inaddr, outaddr, g) ({ \
+    (it1) = delay[((delayp) + (outaddr)) & ((delay_len) - 1)]; \
+    (acc) += ((it1) * -(g)); \
+    delay[((delayp) + (inaddr)) & ((delay_len) - 1)] = (acc); \
+    (acc) = ((acc) * (g)) + (it1); \
+})
+
+// run a first-order all-pass filter with fractional delay read
+// make sure there are enough samples so you don't run off the end
+// delayp - the delay pointer to be updated
+// delay_len - the delay len (must be a power of 2)
+// inaddr - the address to write to
+// outaddr - the address to read from as a float - real = addr, fract = interp
+// g - AP feedback coeff
+// acc - used for input and output
+// it1 - internal temp var - does not need to be retained
+// it2 - internal temp var - does not need to be retained
+#define DSP_UTILS_ALLPASS_FRACT(delay, delayp, delay_len, inaddr, outaddr, g) ({ \
+    (it2) = (outaddr) - (int)(outaddr); \
+    (it1) = delay[((delayp) + (int)(outaddr)) & ((delay_len) - 1)] * (1.0 - (it2)); \
+    (it1) += delay[((delayp) + ((int)(outaddr) + 1)) & ((delay_len) - 1)] * (it2); \
+    (acc) += ((it1) * -(g)); \
+    delay[((delayp) + (inaddr)) & ((delay_len) - 1)] = (acc); \
+    (acc) = ((acc) * (g)) + (it1); \
 })
 
 // convert a factor to a dB value - 1.0 = 0dB (field size)
